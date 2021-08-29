4a607930729163be423da5f77859d6e8ce5c54b5
xivilay <xivilay@gmail.com>
2021-08-29 13:36:15 +0200

    added android libs

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
+mkdir build
 cd /d build
 for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 cmake -A x64 ../ && msbuild /p:Configuration=Release MidiChordRecognition.sln
