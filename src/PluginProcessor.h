f78427957c04f83e9153d9da4a5d9f2030b7010d
xivilay <xivilay@gmail.com>
2021-10-16 21:20:31 +0200

    refactored to class

--- src/PluginProcessor.h
+++ src/PluginProcessor.h
@@ -1,39 +1,77 @@
-#pragma once
-
 #include <JuceHeader.h>
 
 class ChordProcessor : public AudioProcessor, private Timer {
    public:
-    ChordProcessor();
-    ~ChordProcessor() override;
+    ChordProcessor() : AudioProcessor(BusesProperties()) {}
+    ~ChordProcessor() { stopTimer(); }
 
-    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
-    void releaseResources() override;
+    const String getName() const { return JucePlugin_Name; }
 
-    bool isBusesLayoutSupported(const BusesLayout& layouts) const override;
+    bool acceptsMidi() const { return true; }
+    bool producesMidi() const { return false; }
+    bool isMidiEffect() const { return false; }
 
-    void processBlock(AudioBuffer<float>&, MidiBuffer&) override;
+    double getTailLengthSeconds() const { return 0.0; }
+    int getNumPrograms() { return 1; }
+    int getCurrentProgram() { return 0; }
+    void setCurrentProgram(int) {}
+    const String getProgramName(int) { return {}; }
+    void changeProgramName(int, const String&) {}
+    void prepareToPlay(double, int) {}
+    void releaseResources() {}
 
-    AudioProcessorEditor* createEditor() override;
-    bool hasEditor() const override;
+    bool isBusesLayoutSupported(const BusesLayout&) const { return false; }
 
-    const String getName() const override;
+    void processBlock(AudioBuffer<float>& buffer, MidiBuffer& midiMessages) {
+        ScopedNoDenormals noDenormals;
 
-    bool acceptsMidi() const override;
-    bool producesMidi() const override;
-    bool isMidiEffect() const override;
-    double getTailLengthSeconds() const override;
+        buffer.clear();
 
-    int getNumPrograms() override;
-    int getCurrentProgram() override;
-    void setCurrentProgram(int index) override;
-    const String getProgramName(int index) override;
-    void changeProgramName(int index, const String& newName) override;
+        for (const auto metadata : midiMessages) {
+            auto message = metadata.getMessage();
+            short num = message.getNoteNumber();
 
-    void getStateInformation(MemoryBlock& destData) override;
-    void setStateInformation(const void* data, int sizeInBytes) override;
+            if (message.isNoteOn()) {
+                notes.insert(num);
+            } else if (message.isNoteOff()) {
+                notes.erase(num);
+            }
+        }
+    }
 
-    void timerCallback() override;
+    bool hasEditor() const { return true; }
+    AudioProcessorEditor* createEditor() {
+        File exeDir = File::getSpecialLocation(File::currentExecutableFile).getParentDirectory();
+        File bundle = exeDir.getChildFile("js/main.js");
+
+        auto* editor = new reactjuce::GenericEditor(*this, bundle);
+
+        short width = 740;
+        short height = 840;
+        short updateInterval = 25;
+
+        editor->setResizable(true, true);
+        editor->setResizeLimits(width, height, width * 2, height * 2);
+        editor->getConstrainer()->setFixedAspectRatio(width / height);
+        editor->setSize(width, height);
+
+        startTimer(updateInterval);
+
+        return editor;
+    }
+
+    void timerCallback() {
+        if (auto* editor = dynamic_cast<reactjuce::GenericEditor*>(getActiveEditor())) {
+            std::string multi;
+
+            for (const short& x : notes) multi += std::to_string(x) + ",";
+
+            editor->getReactAppRoot().dispatchEvent("activeNotesChanged", static_cast<String>(multi));
+        }
+    }
+
+    void getStateInformation(MemoryBlock&) {}
+    void setStateInformation(const void*, int) {}
 
    private:
     std::unordered_set<short> notes;
